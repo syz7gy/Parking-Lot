6cdb3e96c369155f412c1e96c2441206d4e19cd4
syz7gy <juanpadulsa@gmail.com>
2023-09-01 01:45:50 -0500

    Merge de clases
    Archivos txt reseteados y controllador vacío.

--- ParkingLot_Dulcey_Guerrero/src/co/edu/unbosque/controller/Controller.java
+++ ParkingLot_Dulcey_Guerrero/src/co/edu/unbosque/controller/Controller.java
@@ -1,5 +1,26 @@
 package co.edu.unbosque.controller;
 
 public class Controller {
+<<<<<<< Updated upstream
+=======
+	
+	private Console con;
+	private VehiculoDAO vdao;
+	
+	public Controller() {
+		con = new Console();
+		vdao = new VehiculoDAO();
+		
+		ejecutar();
+		
+	}
+	
+	public void ejecutar() {
+		
+		
+	}
+	
+	
+>>>>>>> Stashed changes
 
 }
